f152fdea818b735f1c5abeb694832660f14c3ade
jrubix <34781690+jrubix@users.noreply.github.com>
2019-12-12 23:03:16 -0800

    Add files via upload

--- simulator.cpp
+++ simulator.cpp
@@ -9,7 +9,7 @@ class Process
 {
 public:
 	Process(int pid, int arrivalTime, int runTime, vector<int> memVector)
-	:pid_(pid), arrivalTime_(arrivalTime), runTime_(runTime), memVector_(memVector) 
+	:pid_(pid), arrivalTime_(arrivalTime), runTime_(runTime), memVector_(memVector)
 	{
 		int c = 0;
 		for(int i = 0; i < memVector_.size(); i++)
@@ -135,7 +135,7 @@ int main()
 		//add event function
 		MemEntry m = new MemEntry(pnum, true); //this stuff might have to all go in the for loop. im actually positive it does
 		list<MemEntry> mList = {m};
-		pair<map<int,list<MemEntry>>::iterator, bool> mem = events.insert(pair<int,list<MemEntry>>(tempArrivalTime,mList)); 
+		pair<map<int,list<MemEntry>>::iterator, bool> mem = events.insert(pair<int,list<MemEntry>>(tempArrivalTime,mList));
 		//insert returns a pair of (iterator to the inserted elemdent, boolean of whether it was inserted correctly or not. returns false if element already exists)
 		//returns false if an equivalent key is already in the table
 		//keys for this insert is arrival time
@@ -171,7 +171,86 @@ int main()
 		load_from_queue(t);
 		t += 100;
 	}
-	
+
+//FUNCTIONS: JUSTIN ****************************************************
+//Load_from_queue				FREE_PAGES VARIABLE UNACCOUNTED FOR
+list<int>::iterator iter = inputQueue_.begin(); // check queue
+		while (iter != inputQueue_.end()) {
+			//////////
+			if (processVector_[*iter].pagesRequired(pageSize_) <= free_pages) { //if enough space add to mem, if not check next
+
+				//MM_add(*iter, t); // REPLACED WITH MM_ADD FUNCTION
+				cout << "\tMM moves Process " << k + 1 << " to memory" << endl<<"\t";
+				size_t size = processVector_[k].pagesRequired(pageSize_);
+				free_pages -= size;
+				int i = 0;
+
+				while (size > 0) {
+					if (pageVector_[i] == -1) {// if page is free else check next page
+						pageVector_[i] = k;
+						processVector_[k].pages_used.push_back(i);
+						size--;
+					}
+					i++;
+				}
+				add_event(k, time + processVector_[k].run_time, false); // REPLACE????
+					/*	pair<bool, int> temp(action, k);
+						list<pair<bool, int>> a = { temp };
+
+						pair<map<int, list<pair<bool, int>>>::iterator, bool> mem
+						= events.insert(pair<int, list<pair<bool, int>>>(t, a));
+						if (!mem.second)
+						mem.first->second.push_back(temp); */
+				//////////////////////////
+
+				iter = inputQueue_.erase(iter);
+
+				//print_queue(); // REPLACED
+						list<int>::const_iterator iter = inputQueue_.cbegin();
+						list<int>::const_iterator iter_end = inputQueue_.cend();
+
+						cout << "Input Queue:[ ";
+						while (iter != iter_end) {
+							cout << *iter + 1 << " ";
+							iter++;
+						}
+						cout << "]" << endl<<"\t";
+				//ORIGINALLY PRINT_MEM()CALL
+				//Print_mem
+				size_t mem_size = pageVector_.size();
+				int start, tail;
+				cout << "Memory Map: ";
+						for (size_t i = 0; i < mem_size; i++) {
+							if (pageVector_[i] != -1) {
+								cout << i * pageSize_ << " - " << i * pageSize_ + pageSize_ -1 <<": Process "
+								<< pageVector_[i] + 1 << ", Page " << processVector_[pageVector_[i]].findPage(i) << endl<<"\t\t";
+							}
+							else { // if page is empty loop til we find a filled page
+								tail = i;
+								start = i;
+									while (i < mem_size && pageVector_[i] == -1) {
+										tail = i;
+										i++;
+									}
+								i--;
+								cout << (start) * pageSize_ << " - " << (tail) * pageSize_ + pageSize_ - 1 << ": Free frame(s)"
+								<< endl<<"\t\t";
+							}
+						}
+						cout<<endl;
+				//END PRINT MEM
+			}
+			else
+				++iter;
+		}
+
+//Virtual_clock
+
+
+
+
+//****************************************************************************
+
 
 
 }
